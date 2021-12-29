1bcb426a9669d44a55b84f4154c437e6469a9ada
loinsir <a9327370@gmail.com>
2021-12-30 00:19:13 +0900

    2309

--- Swift/2309.swift
+++ Swift/2309.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 var heights: [Int] = []
 
 for _ in 1...9 {
@@ -6,7 +8,27 @@ for _ in 1...9 {
 }
 
 for i in 0...7 {
-    for j in i<..8 {
-        var tempH = heights.remo
+    for j in (i+1)...8 {
+        
+        var sum = 0
+        for index in 0...8 {
+            if index == i || index == j {
+                continue
+            }
+
+            sum += heights[index]
+        }
+
+        if sum == 100 {
+            let remove_num = [heights[i], heights[j]]
+            heights.sort()
+            for i in 0...8 {
+                if heights[i] == remove_num[0] || heights[i] == remove_num[1] {
+                    continue
+                }
+                print(heights[i])
+            }
+            exit(0)
+        }
     }
 }
