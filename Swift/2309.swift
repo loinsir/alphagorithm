d492711ff02c7a184b5d71c27d7f72202773a4fe
loinsir <a9327370@gmail.com>
2021-12-29 23:59:12 +0900

    commit

---
+++ Swift/2309.swift
@@ -0,0 +1,12 @@
+var heights: [Int] = []
+
+for _ in 1...9 {
+    let h = Int(readLine()!)!
+    heights.append(h)
+}
+
+for i in 0...7 {
+    for j in i<..8 {
+        var tempH = heights.remo
+    }
+}
