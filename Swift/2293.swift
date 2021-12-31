ac78d9e903bd5efffb73f43d95a9fea32b1527c3
loinsir <a9327370@gmail.com>
2021-12-31 22:19:49 +0900

    2293

---
+++ Swift/2293.swift
@@ -0,0 +1,25 @@
+var input = readLine()!.split(separator: " ")
+let n = input[0]
+let k = input[1]
+
+var coins = [Int]()
+for _ in 1...n {
+    let coin = Int(readLine()!)!
+    coins.append(coin)
+}
+
+var dp = [Int](repeating: 0, count: k+1)
+dp[0] = 1
+
+for coin in coins {
+    for j in 1...k {
+        if coin <= j {
+            dp[j] += dp[j-coin]
+            if dp[j] > Int(pow(2.0, 31.0)) {
+                dp[j] = 0
+            }
+        }
+    }
+}
+
+print(dp[k])
