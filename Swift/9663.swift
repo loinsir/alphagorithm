abdb794a85f98612b3e147e2ce8ae76260bfb38f
loinsir <a9327370@gmail.com>
2022-12-07 01:10:29 +0900

    9663 수정

--- Swift/9663.swift
+++ Swift/9663.swift
@@ -1,30 +1,35 @@
-let N = Int(readLine()!)!
-var board = Array(repeating: -1, count: N)
+let n = Int(readLine()!)!
 
-func promising(_ cdx: Int) -> Bool {
-    for i in 0..<cdx {
-        if board[cdx] == board[i] || cdx - i == abs(board[cdx] - board[i])  {
-            return false
+func solution(_ n:Int) -> Int {
+    var chess = [Int].init(repeating: -1, count: n)
+    var answer = 0
+
+    func check(_ x: Int, _ y: Int) -> Bool {
+        for i in 0..<x {
+            if chess[i] == y || abs(x - i) - abs(y - chess[i]) == 0 { return false }
+        }
+
+        return true
+    }
+
+    func dfs(_ cnt: Int) {
+        if cnt == n {
+            answer += 1
+            return
+        }
+
+        for i in 0..<n {
+            if check(cnt, i) {
+                chess[cnt] = i
+                dfs(cnt + 1)
+                chess[cnt] = -1
+            }
         }
     }
 
-    return true
+    dfs(0)
+
+    return answer
 }
 
-var count = 0
-func nqueen(_ cdx: Int) {
-    if cdx == N {
-        count += 1
-        return
-    }
-
-    for i in 0..<N {
-        board[cdx] = i
-
-        if promising(cdx) {
-            nqueen(cdx + 1)
-        }
-    }
-}
-nqueen(0)
-print(count)
+print(solution(n))
