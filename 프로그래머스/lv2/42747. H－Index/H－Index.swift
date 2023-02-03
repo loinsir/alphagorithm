6081c68ea259c653262ce4e280caaf292bfe7fe9
Inhwan Kim <a9327370@gmail.com>
2023-02-03 20:09:04 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.swift
+++ 프로그래머스/lv2/42747. H－Index/H－Index.swift
@@ -1,13 +1,18 @@
 import Foundation
 
 func solution(_ citations:[Int]) -> Int {
-    var citations = citations.sorted(by: > )
+    if citations.count == 1 {
+        return citations.first!
+    }
+    
+    var answer = 0
+    var citations = citations.sorted(by: >)
     
     for i in 0..<citations.count {
-        if citations[i] <= i {
-            return i
+        if citations[i] <= citations.count - i - 1 && citations[i] >= i+1 {
+            answer = max(answer, citations[i])
         }
     }
     
-    return citations.count
+    return answer
 }
