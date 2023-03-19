bfaf222dfe82bf91f300a1388f0a3f754f61e354
Inhwan Kim <a9327370@gmail.com>
2023-03-19 23:41:59 +0900

    [Gold V] Title: 선 긋기, Time: 404 ms, Memory: 132136 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.swift
@@ -0,0 +1,93 @@
+import Foundation
+
+import Foundation
+
+final class FileIO {
+    
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+let N = file.readInt()
+
+var arr =  [(Int, Int)]()
+
+for _ in 1...N {
+    var lineFirst = file.readInt()
+    var lineSecond = file.readInt()
+    if lineFirst > lineSecond {
+        swap(&lineFirst, &lineSecond)
+    }
+    arr.append((lineFirst, lineSecond))
+}
+
+arr.sort(by: { $0.0 < $1.0 })
+var result = 0
+var l = Int.min, r = Int.min
+
+for i in 0..<N {
+    if r < arr[i].0 {
+        result += r - l
+        l = arr[i].0
+        r = arr[i].1
+    } else {
+        r = max(r, arr[i].1)
+    }
+}
+
+result += r - l
+print(result)
