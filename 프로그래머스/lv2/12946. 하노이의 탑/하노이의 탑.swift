5705d9b0f5a247543e4c552f2396bb4694801535
Inhwan Kim <a9327370@gmail.com>
2023-04-17 14:21:49 +0900

    [level 2] Title: 하노이의 탑, Time: 4.51 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.swift
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-var answer = [[Int]]()
-
 func solution(_ n:Int) -> [[Int]] {
-    hanoi(n, 1, 3, 2)
-    return answer
-}
-
-func hanoi(_ n: Int, _ start: Int, _ to: Int, _ via: Int) {
-    if n == 1 {
-        answer.append([start, to])
-        return
-    } else {
-        hanoi(n-1, start, via, to)
-        answer.append([start, to])
-        hanoi(n-1, via, to, start)
-    }
+    return []
 }
