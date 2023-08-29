fbb2c9f2a1eaefb63507d6960973d3c0844c8b97
Inhwan Kim <a9327370@gmail.com>
2023-08-29 16:45:20 +0900

    [unrated] Title: 롤케이크 자르기, Time: 1765.99 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.swift
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.swift
@@ -1,36 +1,5 @@
 import Foundation
 
 func solution(_ topping:[Int]) -> Int {
-    var answer = 0
-    var toppingDict = [Int: Int]()
-    var brotherDict = [Int: Int]()
-    
-    for top in topping {
-        if toppingDict[top] == nil {
-            toppingDict[top] = 1
-        } else {
-            toppingDict[top]! += 1
-        }
-    }
-    
-    for i in stride(from: topping.count - 1, through: 0, by: -1) {
-        
-        if brotherDict[topping[i]] == nil {
-            brotherDict[topping[i]] = 1
-        } else {
-            brotherDict[topping[i]]! += 1
-        }
-        
-        toppingDict[topping[i]]! -= 1
-        
-        if toppingDict[topping[i]]! == 0 {
-            toppingDict[topping[i]] = nil
-        }
-        
-        if toppingDict.keys.count == brotherDict.keys.count {
-            answer += 1
-        }
-    }
-    
-    return answer
+    return -1
 }
