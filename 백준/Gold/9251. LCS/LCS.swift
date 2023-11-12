9b02d259df091dc964dfc061a88ba56c43b6593e
Inhwan Kim <a9327370@gmail.com>
2023-11-12 23:46:30 +0900

    [Gold V] Title: LCS, Time: 44 ms, Memory: 76984 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.swift
+++ 백준/Gold/9251. LCS/LCS.swift
@@ -1,16 +1,16 @@
-let str1 = readLine()!.map { $0 }
-let str2 = readLine()!.map { $0 }
+let first = readLine()!.map { $0 }
+let second = readLine()!.map { $0 }
 
-var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: str2.count+1), count: str1.count + 1)
+var dp = [[Int]](repeating: [Int](repeating: 0, count: second.count + 1), count: first.count + 1)
 
-for i in 1...str1.count {
-    for j in 1...str2.count {
-        if str1[i-1] == str2[j-1] {
+for i in 1...first.count {
+    for j in 1...second.count {
+        if first[i - 1] == second[j - 1] {
             dp[i][j] = dp[i-1][j-1] + 1
         } else {
-            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         }
     }
 }
 
-print(dp[str1.count][str2.count])
+print(dp[first.count][second.count])
