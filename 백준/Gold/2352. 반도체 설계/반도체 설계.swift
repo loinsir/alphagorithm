8ebd0cf4a4000af91ecd5b7d99e7bc1cb500fb65
Inhwan Kim <a9327370@gmail.com>
2024-01-12 19:19:28 +0900

    [Gold II] Title: 반도체 설계, Time: 36 ms, Memory: 82912 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+var lis = [arr[0]]
+
+for i in 0..<n {
+    if lis.last! < arr[i] {
+        lis.append(arr[i])
+    } else {
+        var start = 0, end = lis.count
+        while start < end {
+            let mid = (start + end) / 2
+            if lis[mid] < arr[i] {
+                start = mid + 1
+            } else {
+                end = mid
+            }
+        }
+        lis[end] = arr[i]
+    }
+}
+
+print(lis.count)
