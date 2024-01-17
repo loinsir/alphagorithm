171fbcc7bac501c34d2c37acc539a1b5a90c9871
Inhwan Kim <a9327370@gmail.com>
2024-01-17 16:36:44 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 12 ms, Memory: 80308 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.swift
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.swift
@@ -3,6 +3,7 @@ import Foundation
 let n = Int(readLine()!)!
 let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
 
+let order = [9, 3, 1]
 var answer = Int.max
 
 var dp = [[[Int]]](repeating: [[Int]](repeating: [Int](repeating: 61, count: 61), count: 61), count: 61)
@@ -13,34 +14,18 @@ func attack(_ num1: Int, _ num2: Int, _ num3: Int, _ count: Int) {
         return
     }
 
-    if dp[max(num1-9, 0)][max(num2-3, 0)][max(num3-1, 0)] > count + 1 {
-        dp[max(num1-9, 0)][max(num2-3, 0)][max(num3-1, 0)] = count + 1
-        attack(max(num1-9, 0), max(num2-3, 0), max(num3-1, 0), count + 1)
-    }
-
-    if dp[max(num1-9, 0)][max(num2-1, 0)][max(num3-3, 0)] > count + 1 {
-        dp[max(num1-9, 0)][max(num2-1, 0)][max(num3-3, 0)] = count + 1
-        attack(max(num1 - 9, 0), max(num2 - 1, 0), max(num3 - 3, 0), count + 1)
-    }
-
-    if dp[max(num1-3, 0)][max(num2-1, 0)][max(num3-9, 0)] > count + 1 {
-        dp[max(num1-3, 0)][max(num2-1, 0)][max(num3-9, 0)] = count + 1
-        attack(max(num1 - 3, 0), max(num2 - 1, 0), max(num3 - 9, 0), count + 1)
-    }
-
-    if dp[max(num1-3, 0)][max(num2-9, 0)][max(num3-1, 0)] > count + 1 {
-        dp[max(num1-3, 0)][max(num2-9, 0)][max(num3-1, 0)] = count + 1
-        attack(max(num1 - 3, 0), max(num2 - 9, 0), max(num3 - 1, 0), count + 1)
-    }
-
-    if dp[max(num1-1, 0)][max(num2-3, 0)][max(num3-9, 0)] > count + 1 {
-        dp[max(num1-1, 0)][max(num2-3, 0)][max(num3-9, 0)] = count + 1
-        attack(max(num1 - 1, 0), max(num2 - 3, 0), max(num3 - 9, 0), count + 1)
-    }
-
-    if dp[max(num1-1, 0)][max(num2-9, 0)][max(num3-3, 0)] > count + 1 {
-        dp[max(num1-1, 0)][max(num2-9, 0)][max(num3-3, 0)] = count + 1
-        attack(max(num1 - 1, 0), max(num2 - 9, 0), max(num3 - 3, 0), count + 1)
+    for i in 0..<3 {
+        for j in 0..<3 where j != i {
+            for k in 0..<3 where k != i && k != j {
+                let nNum1 = max(num1 - order[i], 0)
+                let nNum2 = max(num2 - order[j], 0)
+                let nNum3 = max(num3 - order[k], 0)
+                if dp[nNum1][nNum2][nNum3] > count + 1 {
+                    dp[nNum1][nNum2][nNum3] = count + 1
+                    attack(nNum1, nNum2, nNum3, count + 1)
+                }
+            }
+        }
     }
 }
 
