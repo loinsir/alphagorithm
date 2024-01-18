b95a595c69fc23624400f83ed9b3cf8298ead037
Inhwan Kim <a9327370@gmail.com>
2024-01-19 07:32:00 +0900

    [Gold IV] Title: 해킹, Time: 1084 ms, Memory: 84452 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.swift
+++ 백준/Gold/10282. 해킹/해킹.swift
@@ -1,8 +1,7 @@
 import Foundation
 
 let t = Int(readLine()!)!
-var graph = [[(Int, Int)]]()
-var distances = [Int](repeating: Int.max, count: n + 1)
+var graph = [Int: [Int: Int]]()
 
 var n = 0
 var d = 0
@@ -13,21 +12,19 @@ for _ in 0..<t {
     let ndc = readLine()!.split(separator: " ").map { Int($0)! }
     (n, d, c) = (ndc[0], ndc[1], ndc[2])
     
-    graph = [[(Int, Int)]](repeating: [(Int, Int)](), count: n + 1)
-    
+    graph = [Int: [Int: Int]]()
     for _ in 0..<d {
         let abs = readLine()!.split(separator: " ").map { Int($0)! }
         let (a, b, s) = (abs[0], abs[1], abs[2])
-        graph[b].append((a, s))
+        graph[b, default: [Int: Int]()][a] = s
     }
     
-    distances = [Int](repeating: Int.max, count: n + 1)
-    dijkstra(start: c)
+    let distances = dijkstra(start: c)
     var computers = 0
     var seconds = 0
 
     for i in 1...n {
-        if distances[i] < Int.max {
+        if distances[i] != Int.max {
             computers += 1
             seconds = max(seconds, distances[i])
         }
@@ -36,39 +33,39 @@ for _ in 0..<t {
     print(computers, seconds)
 }
 
-final class Heap<T: Comparable> {
+class Heap<T: Comparable> {
     private var nodes: [T] = []
     private let sort: (T, T) -> Bool
     
-    init(sort: @escaping ((T, T) -> Bool)) {
+    init(sort: @escaping (T, T) -> Bool) {
         self.sort = sort
     }
     
     var isEmpty: Bool {
-        nodes.isEmpty
+        return nodes.isEmpty
     }
     
     func insert(_ data: T) {
         var index = nodes.count
         nodes.append(data)
         
-        while index >= 0, sort(nodes[index], nodes[(index-1) / 2]) {
+        while index > 0 && sort(nodes[index], nodes[(index - 1) / 2]) {
             nodes.swapAt(index, (index - 1) / 2)
-            index = (index-1) / 2
+            index = (index - 1) / 2
         }
     }
     
     func delete() -> T {
         if nodes.count == 1 {
-            return nodes.removeFirst()
+            return nodes.removeLast()
         }
         
         let data = nodes.first!
         nodes.swapAt(0, nodes.count - 1)
         nodes.removeLast()
         
-        let limit = nodes.count
         var index = 0
+        let limit = nodes.count
         
         while index < limit {
             let leftChild = index * 2 + 1
@@ -101,26 +98,30 @@ struct Node: Comparable {
     }
 }
 
-func dijkstra(start: Int) {
-    var pq = [Node]()
-    var visited = [Bool](repeating: false, count: n + 1)
-    pq.append(Node(start, 0))
+func dijkstra(start: Int) -> [Int] {
+    let pq = Heap<Node>(sort: <)
+    var distances = [Int](repeating: Int.max, count: n + 1)
+    
+    pq.insert(Node(start, 0))
     distances[start] = 0
     
     while !pq.isEmpty {
-        pq.sort { $0.dist > $1.dist }
-        let node = pq.removeLast()
-        
+        let node = pq.delete()
         let currentNode = node.point, currentDist = node.dist
-        visited[currentNode] = true
         
-        for (key, value) in graph[currentNode] {
+        if currentDist > distances[currentNode] {
+            continue
+        }
+        
+        for (key, value) in graph[currentNode, default: [Int : Int]()] {
             let nextDist = currentDist + value
-            if visited[key] { continue }
+
             if nextDist < distances[key] {
                 distances[key] = nextDist
-                pq.append(Node(key, nextDist))
+                pq.insert(Node(key, nextDist))
             }
         }
     }
+
+    return distances
 }
