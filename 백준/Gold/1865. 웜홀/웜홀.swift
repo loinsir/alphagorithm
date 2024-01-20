6aa4efc95a3b3901f297ae05c643dbb5583f589e
Inhwan Kim <a9327370@gmail.com>
2024-01-20 16:14:21 +0900

    [Gold III] Title: 웜홀, Time: 96 ms, Memory: 69496 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.swift
+++ 백준/Gold/1865. 웜홀/웜홀.swift
@@ -23,50 +23,24 @@ for _ in 0..<tc {
         edges.append([s, e, -t])
     }
 
-    var flag = false
-    for i in 1...n {
-        if belmanFord(i) {
-            flag = true
-            break
-        }
-    }
-
-    answer += flag ? "YES\n" : "NO\n"
+    answer += belmanFord(1) ? "YES\n" : "NO\n"
 }
 
 print(answer)
 
 func belmanFord(_ start: Int) -> Bool {
-    distances = [Int](repeating: Int.max, count: n+1)
+    distances = [Int](repeating: 10001, count: n+1)
     distances[start] = 0
-    var visitedFlag = false
 
-    for v in 1..<n {
-        visitedFlag = false
+    for v in 1...n {
         for i in 0..<2*m+w {
             let currentNode = edges[i][0]
             let nextNode = edges[i][1]
             let cost = edges[i][2]
 
-            if distances[currentNode] != Int.max && distances[nextNode] > distances[currentNode] + cost {
+            if distances[nextNode] > distances[currentNode] + cost {
                 distances[nextNode] = distances[currentNode] + cost
-                visitedFlag = true
-            }
-        }
-
-        if !visitedFlag {
-            break
-        }
-    }
-
-    if visitedFlag {
-        for i in 0..<2*m+w {
-            let currentNode = edges[i][0]
-            let nextNode = edges[i][1]
-            let cost = edges[i][2]
-
-            if distances[currentNode] != Int.max && distances[nextNode] > distances[currentNode] + cost {
-                return true
+                if v == n { return true }
             }
         }
     }
