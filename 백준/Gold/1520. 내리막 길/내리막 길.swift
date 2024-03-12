25d7d1882c998328f0ff929c4ad1421b585baa06
Inhwan Kim <a9327370@gmail.com>
2024-03-13 00:32:43 +0900

    [Gold III] Title: 내리막 길, Time: 108 ms, Memory: 83572 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.swift
+++ 백준/Gold/1520. 내리막 길/내리막 길.swift
@@ -1,40 +1,43 @@
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-let (m, n) = (input[0], input[1])
+let mn = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (m, n) = (mn[0], mn[1])
 
-var map = [[Int]]()
+var board = [[Int]]()
+
 for _ in 0..<m {
     let row = readLine()!.split(separator: " ").map { Int(String($0))! }
-    map.append(row)
+    board.append(row)
 }
+
+var dp = [[Int]](repeating: [Int](repeating: 0, count: n), count: m)
 var answer = 0
 let dy = [-1, 0, 1, 0], dx = [0, 1, 0, -1]
 
-var dp = [[Int]](repeating: [Int](repeating: 0, count: n), count: m)
 func dfs(_ y: Int, _ x: Int) -> Int {
-    if y == m - 1 && x == n - 1 {
+    if y == m-1 && x == n-1 {
         return 1
     }
-    
+
     for i in 0..<4 {
         let ny = y + dy[i], nx = x + dx[i]
-        
-        if 0..<m ~= ny && 0..<n ~= nx && map[ny][nx] < map[y][x] {
-            if dp[ny][nx] > 0 { // dp > 0이면 갈 수 있다는 뜻
+
+        if 0..<m ~= ny && 0..<n ~= nx && board[ny][nx] < board[y][x] {
+            if dp[ny][nx] > 0 {
                 dp[y][x] += dp[ny][nx]
-            } else if dp[ny][nx] == 0 { // dp == 0 이면 아직 탐색 안했다는 뜻
+            } else if dp[ny][nx] == 0 {
                 dp[y][x] += dfs(ny, nx)
-            } // dp < 0 이면 도달할 수 없다는 뜻
+            }
         }
     }
-    
-    if dp[y][x] == 0 { // 지금 현재 있는 곳은 도착 지점에 도달 못하는 곳
+
+    if dp[y][x] == 0 {
         dp[y][x] -= 1
         return 0
     } else {
         return dp[y][x]
     }
 }
+
 answer = dfs(0, 0)
 print(answer == -1 ? 0 : answer)
